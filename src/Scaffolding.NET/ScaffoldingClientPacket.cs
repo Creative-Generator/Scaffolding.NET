5f1a074ee4a51214d7cf872bdaa0bd397cf47fe5
Hill23333 <a18035135032@163.com>
2025-10-21 22:51:32 +0800

    feat: 两种 Packet

---
+++ src/Scaffolding.NET/ScaffoldingClientPacket.cs
@@ -0,0 +1,57 @@
+﻿using System.Buffers.Binary;
+using System.Text;
+
+namespace Scaffolding.NET;
+
+public struct ScaffoldingClientPacket()
+{
+    public ScaffoldingClientPacket(ReadOnlyMemory<byte> source) : this()
+    {
+        Source = source;
+    }
+
+    public ScaffoldingClientPacket(string requestType, byte[] data) : this()
+    {
+        if (RequestType.Length > 255)
+        {
+            throw new ArgumentException("请求类型长度不能大于 255。", nameof(requestType));
+        }
+
+        var typeBytes = Encoding.UTF8.GetBytes(requestType);
+        var totalLength = 1 + typeBytes.Length + 4 + data.Length;
+
+        var buffer = new byte[totalLength];
+        var span = buffer.AsSpan();
+
+        // 写入 RequestTypeLength
+        span[0] = (byte)typeBytes.Length;
+
+        // 写入 RequestType
+        typeBytes.CopyTo(span.Slice(1, typeBytes.Length));
+
+        // 写入 DataLength
+        BinaryPrimitives.WriteUInt32BigEndian(span.Slice(1 + typeBytes.Length, 4), (uint)data.Length);
+
+        // 写入 Data
+        data.CopyTo(span.Slice(1 + typeBytes.Length + 4));
+
+        Source = buffer;
+    }
+
+    public readonly ReadOnlyMemory<byte> Source;
+
+    public byte RequestTypeLength => Source.Span[0];
+
+    public string RequestType
+    {
+        get
+        {
+            var data = Source.Slice(1, RequestTypeLength);
+            return Encoding.UTF8.GetString(data.ToArray());
+        }
+    }
+
+    public uint DataLength => BinaryPrimitives.ReadUInt32BigEndian(Source.Span.Slice(1 + RequestTypeLength, 4));
+
+    public ReadOnlyMemory<byte> Data => Source.Slice(1 + RequestTypeLength + 4, (int)DataLength);
+}
---
+++ src/Scaffolding.NET/ScaffoldingServerPacket.cs
@@ -0,0 +1,38 @@
+﻿using System.Buffers.Binary;
+
+namespace Scaffolding.NET;
+
+public struct ScaffoldingServerPacket
+{
+    public ScaffoldingServerPacket(ReadOnlyMemory<byte> source)
+    {
+        Source = source;
+    }
+
+    public ScaffoldingServerPacket(byte responseStatus, byte[] data)
+    {
+        var totalLength = 1 + 4 + data.Length;
+
+        var buffer = new byte[totalLength];
+        var span = buffer.AsSpan();
+
+        // 写入 ResponseStatus
+        span[0] = responseStatus;
+        
+        // 写入 DataLength
+        BinaryPrimitives.WriteUInt32BigEndian(span.Slice(1, 4), (uint)data.Length);
+
+        // 写入 Data
+        data.CopyTo(span.Slice(1 + 4));
+
+        Source = buffer;
+    }
+
+    public readonly ReadOnlyMemory<byte> Source;
+
+    public byte ResponseStatus => Source.Span[0];
+
+    public uint DataLength => BinaryPrimitives.ReadUInt32BigEndian(Source.Span.Slice(1, 4));
+
+    public ReadOnlyMemory<byte> Data => Source.Slice(1 + 4, (int)DataLength);
+}
