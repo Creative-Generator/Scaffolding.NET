e36107cfbf578296fe6245457bce438deff8b764
Hill23333 <a18035135032@163.com>
2025-10-22 22:57:35 +0800

    chore: IMachineIdProvider

---
+++ src/Scaffolding.NET.Extensions.MachineId/IMachineIdProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace Scaffolding.NET.Extensions.MachineId;
+
+internal interface IMachineIdProvider
+{
+    /// <summary>BIOS / SMBIOS UUID (product_uuid)</summary>
+    string? GetBiosUuid();
+
+    /// <summary>主板序列号</summary>
+    string? GetMotherboardSerial();
+
+    /// <summary>第一个“物理”MAC 地址（去掉虚拟/loopback）</summary>
+    string? GetFirstMac();
+
+    /// <summary>第一个物理磁盘的序列号</summary>
+    string? GetFirstDiskSerial();
+}
