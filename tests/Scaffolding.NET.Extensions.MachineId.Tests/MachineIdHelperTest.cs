1624f3714ba2e50b7865cfad79dc6c8e325d5148
Hill23333 <a18035135032@163.com>
2025-10-22 22:58:00 +0800

    chore: MachineIdHelperTest

---
+++ tests/Scaffolding.NET.Extensions.MachineId.Tests/MachineIdHelperTest.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using Scaffolding.NET.Extensions.MachineId;
+
+namespace Scaffolding.NET.Extensions.MachineId.Tests;
+
+[TestSubject(typeof(MachineIdHelper))]
+public class MachineIdHelperTest(ITestOutputHelper output)
+{
+
+    [Fact]
+    public void TestMachineId()
+    {
+        output.WriteLine(MachineIdHelper.GetMachineId());
+    }
+}
